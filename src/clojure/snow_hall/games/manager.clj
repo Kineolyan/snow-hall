52252843ed1332a5c9a51a666e5067c7b00a610d
Kineolyan <kineolyan@gmail.com>
2019-09-10 21:01:09 +0200

    Add some tests to the project.

--- src/clojure/snow_hall/games/manager.clj
+++ src/clojure/snow_hall/games/manager.clj
@@ -11,12 +11,6 @@
   (dosync
     (alter store assoc (game :name) game)))
 
-(defn remove-game
-  "Removes a game from the server"
-  [store store-name]
-  (dosync
-    (alter store dissoc store-name)))
-
 (defn list-games
   "Lists all games available in this server"
   [store]
