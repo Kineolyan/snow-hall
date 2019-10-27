5594e2f82f453b10193eb96f765cf4d0d391b25d
Kineolyan <kineolyan@gmail.com>
2019-10-27 14:30:33 +0100

    (WIP) joining a gathering.

--- src/clojure/snow_hall/hall/butler.clj
+++ src/clojure/snow_hall/hall/butler.clj
@@ -14,15 +14,40 @@
   []
   {})
 
+(defn create-player-list
+  [player-count first-player]
+  (reduce
+    (fn [acc _] (conj acc {:token (random-uuid)}))
+    [(:id first-player)]
+    (range (dec player-count))))
+
 (defn create-gathering
   "Creates a new game for a user"
-  [tab user game]
+  [{:keys [tab user game]}]
   (let [game-id (generate-id tab)
-        players (reduce
-                  (fn [acc _] (conj acc {:token (random-uuid)}))
-                  [(:id user)]
-                  (range (dec (:player-count game))))
+        players (create-player-list (:player-count game) user)
         new-game {:id game-id
                   :game (:name game)
-                  :players [(:id user)]}]
+                  :players players}]
     (assoc tab game-id new-game)))
+
+(defn get-token-idx
+  [players token]
+  (->> (map :token players)
+       (map vector (range))
+       (filter (partial = token))
+       (ffirst)))
+
+(defn integrate-visitor
+  [game user token]
+  (if-let [i (get-token-idx (:players game) token)]
+    (assoc (:players game) i (:id user))
+    {:error (str "Token not in the gathering list: " token)}))
+
+(defn join-gathering
+  "Joins an existing gathering"
+  [{:keys [tab user gathering-id token]}]
+  (if-let [game (get tab gathering-id)]
+    (integrate-visitor game user token)
+    {:error (str "Not a gathering id " gathering-id)}))
+
--- test/clojure/snow_hall/hall/butler_test.clj
+++ test/clojure/snow_hall/hall/butler_test.clj
@@ -18,25 +18,66 @@
 (deftest create-gathering
   (let [tab {}
         user {:id 1}
-        game {:mame  "g" :player-count 3}
-        updated (m/create-gathering tab user game)]
+        game {:name "g" :player-count 3}
+        updated (m/create-gathering {:tab tab
+                                     :user user
+                                     :game game})]
     (testing "registers a new gathering into the tab"
-      (= (count updated) 1)
-      (= (keys updated) ((comp :id first vals) updated)))
+      (is (= (count updated) 1))
+      (is (= (keys updated)
+             (map :id (vals updated)))))
 
     (testing "records the game to play"
       (let [g (first (vals updated))]
-        (= (:game g) "g")))
+        (is (= (:game g) "g"))))
 
     (testing "includes the user as first player"
       (let [g (first (vals updated))]
-        (= (first (:players g))
-           1)))
+        (is (= (first (:players g))
+               1))))
 
     (testing "creates connection token for the other players"
       (let [g (first (vals updated))
             others (rest (:players g))
             tokens (map :token others)]
-        (= 2 (count (set tokens)))
-        (= true (not-any? nil? tokens))))))
+        (is (= 2 (count (set tokens))))
+        (is (= true (not-any? nil? tokens)))))))
+
+(deftest get-token-idx
+  (let [players [1 {:token "abc"} 2 {:token "def"}]]
+    (testing "finds the index of an existing token"
+      (is (= 1 (m/get-token-idx players "abc")))
+      (is (= 3 (m/get-token-idx players "def"))))
+
+    (testing "returns falsy for an unknown token"
+      (is (not (m/get-token-idx players "ghi"))))))
+
+(deftest join-gathering
+  (let [initial-tab (m/create-gathering
+                      {:tab {}
+                       :user {:id 1}
+                       :game {:name "g" :player-count 3}})
+          gathering (first (vals initial-tab))
+          updated-tab (m/join-gathering
+                        {:tab initial-tab
+                         :user {:id 2}
+                         :gathering-id (:id gathering)
+                         :token (:token (nth (:players gathering) 1))})]
+    (println updated-tab)
+    (testing "adds the visitor to the gathering"
+      (let [updated-gathering (get updated-tab (:id gathering))]
+        (is (some #{2} (:players updated-gathering)))))
+
+    (testing "still contains the initial visitor"
+      (let [updated-gathering (get updated-tab (:id gathering))]
+        (is (some #{1} (:players updated-gathering)))))
+
+    (testing "consumes a token"
+      (let [updated-gathering (get updated-tab (:id gathering))
+            tokens (->> (:players updated-gathering)
+                        (map :token)
+                        (filter (complement nil?)))]
+        (is (= (count tokens) 1))))))
+
+
 
