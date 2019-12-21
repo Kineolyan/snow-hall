8cd83b0467b4bf528368c5fc1ffa2201312d83d1
Kineolyan <kineolyan@gmail.com>
2019-12-21 19:32:41 +0100

    Review the process to edit visitors.

--- src/clojure/snow_hall/hall/visitor.clj
+++ src/clojure/snow_hall/hall/visitor.clj
@@ -1,27 +1,46 @@
 (ns snow-hall.hall.visitor
   (:require
-    [medley.core :refer [random-uuid]]))
+   [medley.core :refer [random-uuid]]))
 
 (defn create-registry [] {})
 
-(defn create-new-user
+(defn create
   "Creates a new user, with only a UUID and its secret token"
   []
-  {
-    :uuid (str (random-uuid))
-    :token (str (random-uuid))})
+  {:uuid (str (random-uuid))
+   :token (str (random-uuid))})
+
+(defn on
+  [registry uuid action]
+  (if-let [user (get registry uuid)]
+    (action registry user)
+    (throw (IllegalArgumentException. (str uuid)))))
+
+(defn validate-token
+  [user token]
+  (= (:token user) token))
+
+(defn edit
+  [registry uuid token action]
+  (on
+   registry
+   uuid
+   (fn [registry user]
+     (if (validate-token user token)
+       (assoc registry uuid (partial action registry))
+       (throw (IllegalArgumentException. (str "Invalid token for " uuid)))))))
 
 (defn set-nickname
   "Sets the nickname of the given user.
   If a nickname was already defined, the current value is erased."
   [registry uuid nickname]
   (if-let [user (get registry uuid)]
-    (assoc
-      registry
-      uuid
-      (assoc user :nickname nickname))
+    (assoc registry
+           uuid
+           (assoc user :nickname nickname))
     (throw (IllegalArgumentException. (str uuid)))))
 
-(defn register-user [registry user]
+(defn register
   "Register a new user into the registry"
+  [registry user]
   (assoc registry (:uuid user) user))
--- test/clojure/snow_hall/hall/visitor_test.clj
+++ test/clojure/snow_hall/hall/visitor_test.clj
@@ -1,24 +1,58 @@
 (ns snow-hall.hall.visitor-test
   (:require
-    [clojure.test :refer :all]
+    [clojure.test :refer [deftest testing is]]
     [snow-hall.hall.visitor :as m]))
 
 (deftest create-registry
   (testing "creates an empty user registry"
     (is (= (m/create-registry) {}))))
 
-(deftest create-new-user
+(deftest create
   (testing "creates user with all info"
-    (let [user (m/create-new-user)]
+    (let [user (m/create)]
       ; We have a UUID and a token but not a name yet
       (is ((comp not nil? :uuid) user))
       (is ((comp not nil? :token) user))
       (is ((comp nil? :name) user))))
 
   (testing "creates many different users"
-    (let [users (repeatedly 10 m/create-new-user)
+    (let [users (repeatedly 10 m/create)
           ids (map :uuid users)]
       (is (= (count (set ids)) 10)))))
+
+(deftest on-visitor
+  (let [users (repeatedly 10 m/create)
+        registry (reduce m/register (m/create-registry) users)
+        user-5 (nth users 5)]
+    (testing "gives access to the wanted user"
+      (m/on
+       registry
+       (:uuid user-5)
+       #(do 
+          (is (= %2 user-5))
+          (is (= %1 registry)))))
+    
+    (testing "throws for non-existing user"
+      (is (thrown? 
+           IllegalArgumentException
+           (m/on registry "something-wrong" nil))))))
+
+(deftest edit-visitor
+  (let [users (repeatedly 10 m/create)
+        registry (reduce m/register (m/create-registry) users)
+        user-5 (nth users 5)]
+    (testing "gives access to the wanted user"
+      (m/on
+       registry
+       (:uuid user-5)
+       #(do
+          (is (= %2 user-5))
+          (is (= %1 registry)))))
+
+    (testing "throws for non-existing user"
+      (is (thrown?
+           IllegalArgumentException
+           (m/on registry "something-wrong" nil))))))
 
 (deftest set-nickname
   (testing "sets once the nickname of the user"
