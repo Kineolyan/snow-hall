2737dd5b9dfdd272122af9c0d1b7978bcedb3e8b
Kineolyan <kineolyan@gmail.com>
2019-12-22 22:18:56 +0100

    Add test on REST.

---
+++ test/clojure/snow_hall/hall/rest_test.clj
@@ -0,0 +1,30 @@
+(ns snow-hall.hall.rest-test
+  (:require
+   [clojure.test :refer [deftest testing is]]
+   [snow-hall.hall.visitor :as visitor]
+   [snow-hall.hall.rest :as m]))
+
+(defn request
+  [body]
+  {:body body})
+
+(deftest register-visitor
+  (let [registry (ref (visitor/create-registry))
+        answer (m/register-visitor-request
+                registry
+                (request {"nickname" "me"}))]
+    (testing "adds a new visitor to the registry"
+      (is (= (->> @registry (vals) (map :nickname))
+             ["me"])))
+    (testing "returns the uuid of the new visitor"
+      (is (contains?
+           @registry
+           (get-in answer [:body :uuid]))))
+    (testing "returns the token of the visitor"
+      (let [{:keys [token uuid]} (:body answer)]
+        (is (= token
+               (get-in @registry [uuid :token])))))
+    (testing "returns the nickname of the visitor"
+      (let [{:keys [uuid nickname]} (:body answer)]
+        (is (= nickname
+               (get-in @registry [uuid :nickname])))))))
