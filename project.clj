4c0302e626da3db2707e47a8d7918b4a2386d50a
Olivier <Kineolyan@users.noreply.github.com>
2019-12-31 15:28:57 +0100

    Improve github pipeline and use Clojure 10 (#5)

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [compojure "1.6.1"]
                  [http-kit "2.3.0"]
                  [ring/ring-defaults "0.3.2"]
