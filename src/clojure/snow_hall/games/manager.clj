5dc530dbcfa16b761318047868afd7a9d926c611
Kineolyan <kineolyan@gmail.com>
2019-12-31 15:47:13 +0100

    Add validation.

--- src/clojure/snow_hall/games/manager.clj
+++ src/clojure/snow_hall/games/manager.clj
@@ -1,9 +1,21 @@
-(ns snow-hall.games.manager)
+(ns snow-hall.games.manager
+  (:require [clojure.spec.alpha :as s]))
+
+; Specs
+
+(s/def ::name string?)
+(s/def ::player-count int?)
+(s/def ::java string?)
+(s/def ::game (s/keys :req [::name ::player-count]
+                      :opt [::java]))
+(s/def ::games (s/map-of ::name ::game))
+
+; methods
 
 (defn create-store
   "Creates a store holding all games that can be played"
   []
-  (ref {}))
+  (ref {} :validator (partial s/valid? ::games)))
 
 (defn add-game
   "Adds a new game to the server"
