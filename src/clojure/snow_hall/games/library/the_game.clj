84dc986751ed7cace670d64d6238b348768291c6
Kineolyan <kineolyan@protonmail.com>
2020-08-15 14:55:35 +0200

    WIP code the reception of messages.

--- src/clojure/snow_hall/games/library/the_game.clj
+++ src/clojure/snow_hall/games/library/the_game.clj
@@ -23,9 +23,27 @@
 
 ; -- Creating the rounds and piping
 
+(defn merge-pos-and-input
+  [pos input]
+  (concat [pos] input))
+
+(defn create-input-io
+  [i]
+  (async/map (partial merge-pos-and-input i) [(chan 1)] 1))
+
+(comment
+  (def c (create-input-io 3))
+  (async/offer! c [45 97])
+  (async/poll! c))
+; FIXME not working yet
+
 (defn create-io
-  []
+  [i]
   {:in (chan 1) :out (chan 1)})
+
+(defn create-ios
+  [{:keys [players]}]
+  (repeatedly players create-io))
 
 (defrecord Round [ios stop state]
   game/RoundEngine
@@ -115,68 +133,11 @@
   [stacks deck]
   (some (partial can-play-card? stacks) deck))
 
-(comment 
-  (can-play-with-deck? {:up [97 98] :down [2 3]} [10 20 30]))
-
 (defn loss?
   [{:keys [current-player decks stacks]}]
-  true)
-
-(defn set-sign-in-state
-  [state player-idx sign]
-  (update-in state [:signs player-idx] (constantly sign)))
-
-(defn winning-sign-over?
-  [s1 s2]
-  (-> win-matrix s1 s2))
-
-(defn get-winner
-  [{:keys [p1 p2]}]
-  (cond
-    (winning-sign-over? p1 p2) :p1
-    (winning-sign-over? p2 p1) :p2))
-
-(defn update-score-in-state
-  [state]
-  (if-let [winner (get-winner (:signs state))]
-    (update-in state [:scores winner] inc)
-    state))
-
-(defn reset-game-in-state
-  [{:keys [signs] :as state}]
-  (assoc state
-         :signs {}
-         :last-signs signs))
-
-(defn find-winner
-  [state]
-  (cond
-    (win? state :p1) :p1
-    (win? state :p2) :p2))
-
-(defn check-for-victory
-  [state]
-  (if-let [winner (find-winner state)]
-    (assoc state
-           :winner winner
-           :messages {:win "WIN BY POINTS"
-                      :loss "LOSS BY POINTS"}
-           :status :ended)
-    state))
-
-(defn all-signs?
-  [{:keys [signs]}]
-  (every? signs [:p1 :p2]))
-
-(defn play-move
-  [state player-id sign]
-  (let [state-with-signs (set-sign-in-state state player-id sign)]
-    (if (all-signs? state-with-signs)
-      (-> state-with-signs
-          update-score-in-state
-          reset-game-in-state
-          check-for-victory)
-      state-with-signs)))
+  (let [player-deck (nth decks current-player)
+        can-play? (can-play-with-deck? stacks player-deck)]
+    (not can-play?)))
 
 (defn mark-round-as-started!
   [round]
@@ -189,38 +150,19 @@
 
 (defn mark-illegal-turn
   [state player]
-  (assoc state
-         :status :ended
-         :winner (if (= player :p1) :p2 :p1)
-         :messages {:win "WIN: OPPONENT ILLEGAL TURN"
-                    :loss "LOSS: NOT YOUR TURN"}))
+  nil)
 
 (defn mark-illegal-move
   [state player]
-  (assoc state
-         :status :ended
-         :winner (if (= player :p1) :p2 :p1)
-         :messages {:win "WIN: OPPONENT ILLEGAL MOVE"
-                    :loss "LOSS: ILLEGAL MOVE"}))
+  nil)
 
 (defn apply-move
-  [{:keys [signs winner] :as state} [player move]]
-  (let [p1-played? (:p1 signs)
-        p2-played? (:p2 signs)]
-    (cond
-      (some? winner) state ; do not change the state once the game ended
-      (and (= player :p1) p1-played?) (mark-illegal-turn state player)
-      (and (= player :p2) p2-played?) (mark-illegal-turn state player)
-      (nil? move) (mark-illegal-move state player)
-      :else (play-move state player move))))
+  [{:keys [] :as state} [player move]]
+  nil)
 
 (defn compute-next-state!
   [round move]
   (swap! (:state round) apply-move move))
-
-(defn publish-state?
-  [next-state]
-  (empty? (:signs next-state)))
 
 ; -- side-effect interaction
 
@@ -238,10 +180,7 @@
 (defn notify-victory!
   "Notifies all players matched by f of the victory, and others of defeat"
   [round f messages]
-  (let [winners (filter f (:ios round))
-        losers (filter (complement f) (:ios round))]
-    (send-message! winners (:win messages))
-    (send-message! losers (:loss messages))))
+  nil)
 
 (defn end-prematurely!
   [round]
@@ -254,14 +193,8 @@
   (doseq [io (:ios round)]
     (>!! (:out io) (state->str state))))
 
-(defn get-winner-io
-  [ios winner]
-  (case winner
-    :p1 (nth ios 0)
-    :p2 (nth ios 1)))
-
 (defn publish-completion!
-  [round {:keys [winner messages]}]
+  [round {:keys []}]
   (notify-victory! round (partial = (get-winner-io (:ios round) winner)) messages)
   (end-game! round))
 
@@ -274,7 +207,7 @@
      [(cond
         (= c in1) :p1
         (= c in2) :p2)
-      (str->sign m)])))
+      (str->cards m)])))
 
 (defn run-loop!
   [round]
@@ -284,16 +217,15 @@
       (if (= move :stop)
         (end-prematurely! round)
         (let [next-state (compute-next-state! round move)]
-          (when (publish-state? next-state)
-            (publish-state! round next-state))
+          (publish-state! round next-state)
           (if (round-completed? round)
             (publish-completion! round next-state)
             (recur)))))))
 
 (defn- create
   [options]
-  (RpclsRound.
-   (repeatedly 2 create-io)
+  (Round.
+   (create-ios options)
    (chan 1)
    (atom (create-state options))))
 
